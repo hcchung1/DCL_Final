1aca2c09262ae384e0e6b1ca2f39da5dd353bc14
周琨棋 <s40131qwer@gmail.com>
2024-12-14 00:29:08 +0800

    Date : 12/14 00:28

--- Final_Project_Snake/lab8.srcs/sources_1/new/Check.v
+++ Final_Project_Snake/lab8.srcs/sources_1/new/Check.v
@@ -38,7 +38,7 @@ module Check(
     localparam b_tall = 120; 
 
     // fsm s_check
-    localparam S_init = 0, S_pre = 1, S_dead = 2, S_apl = 3, S_pos = 4, S_endgame;
+    localparam S_init = 0, S_pre = 1, S_dead = 2, S_apl = 3, S_pos = 4, S_endgame = 5;
     reg [2:0] s_check = 0, s_check_next = 0;    
 
     // S_init index
