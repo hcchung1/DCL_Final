a62fe12980d9f22779b4f65ac095bfd790921815
ericchen1121 <ericjk.cs12@nycu.edu.tw>
2024-12-17 00:22:06 +0800

    Date: 20241217 00:22

--- Final_Project_Snake/lab8.srcs/sources_1/new/Screen.v
+++ Final_Project_Snake/lab8.srcs/sources_1/new/Screen.v
@@ -242,8 +242,8 @@ localparam NUMBER_H = 30;
 localparam NUMBER_W = 25;
 localparam NUMBER1_VPOS = 200;
 localparam NUMBER2_VPOS = 200;
-localparam NUMBER1_HPOS = 605;
-localparam NUMBER2_HPOS = 635;
+localparam NUMBER1_HPOS = 613;
+localparam NUMBER2_HPOS = 639;
 wire [119:0] now_region;
 // assign now_region =
 //            pixel_y >= (Vertical_pos<<1) && pixel_y < (Vertical_pos+FISH_H)<<1 &&
@@ -348,38 +348,39 @@ always @(posedge clk) begin
             if (snake[399:392] != 0) begin
                 length <= length + 1;
                 if (length == 0) begin
-                    if (snake[399:392] == snake[391:384] + 1) begin // head right
+                    if ((snake[399:392] == snake[391:384] + 1) || snake[399:392] == snake[391:384] + 11) begin // head right
                         mark[snake[399:392]-1] <= 0;
-                    end else if (snake[399:392] == snake[391:384] - 1) begin // head left
+                    end else if ((snake[399:392] == snake[391:384] - 1) || snake[399:392] == snake[391:384] - 11) begin // head left
                         mark[snake[399:392]-1] <= 1;
-                    end else if (snake[399:392] == snake[391:384] - 12) begin // head up
+                    end else if ((snake[399:392] == snake[391:384] - 12) || snake[399:392] == snake[391:384] - 108) begin // head up
                         mark[snake[399:392]-1] <= 2;
-                    end else if (snake[399:392] == snake[391:384] + 12) begin // head down
+                    end else if ((snake[399:392] == snake[391:384] + 12) || snake[399:392] == snake[391:384] + 108) begin // head down
                         mark[snake[399:392]-1] <= 3;
                     end
                 end else if (snake[391:384] != 0) begin
-                    if ((snake[399:392] == snake[391:384] + 1 && snake[399:392] == prev_snake - 1) || (snake[399:392] == snake[391:384] - 1 && snake[399:392] == prev_snake + 1) ) // left-right
+                    if ((snake[399:392] == snake[391:384] + 1 && snake[399:392] == prev_snake - 1) || (snake[399:392] == snake[391:384] - 1 && snake[399:392] == prev_snake + 1) || (snake[399:392] == snake[391:384] + 1 && snake[399:392] == prev_snake + 11) || (snake[399:392] == snake[391:384] + 1 && snake[399:392] == prev_snake - 1) || (snake[399:392] == snake[391:384] - 11 && snake[399:392] == prev_snake - 1) || (snake[399:392] == snake[391:384] - 1 && snake[399:392] == prev_snake - 11)) // left-right
                         mark[snake[399:392]-1] <= 5;
-                    else if ((snake[399:392] == snake[391:384] + 12 && snake[399:392] == prev_snake - 12) || (snake[399:392] == snake[391:384] - 12 && snake[399:392] == prev_snake + 12)) // up-down 
+                    else if ((snake[399:392] == snake[391:384] + 12 && snake[399:392] == prev_snake - 12) || (snake[399:392] == snake[391:384] - 12 && snake[399:392] == prev_snake + 12) || (snake[399:392] == snake[391:384] + 12 && snake[399:392] == prev_snake - 108) || (snake[399:392] == snake[391:384] - 108 && snake[399:392] == prev_snake + 12) || (snake[399:392] == snake[391:384] - 12 && snake[399:392] == prev_snake + 108) || (snake[399:392] == snake[391:384] + 108 && snake[399:392] == prev_snake - 12)) // up-down 
                         mark[snake[399:392]-1] <= 4;
-                    else if ((snake[399:392] == snake[391:384] - 1 && snake[399:392] == prev_snake - 12) || (snake[399:392] == snake[391:384] - 12 && snake[399:392] == prev_snake - 1)) // right-up / down-left
+                    else if ((snake[399:392] == snake[391:384] - 1 && snake[399:392] == prev_snake - 12) || (snake[399:392] == snake[391:384] - 12 && snake[399:392] == prev_snake - 1) || (snake[399:392] == snake[391:384] - 1 && snake[399:392] == prev_snake + 108) || (snake[399:392] == snake[391:384] + 108 && snake[399:392] == prev_snake - 1) || (snake[399:392] == snake[391:384] - 12 && snake[399:392] == prev_snake + 11) || (snake[399:392] == snake[391:384] + 11 && snake[399:392] == prev_snake - 12)) // left-down / up-right (2)
                         mark[snake[399:392]-1] <= 12;
-                    else if ((snake[399:392] == snake[391:384] - 1 && snake[399:392] == prev_snake + 12) || (snake[399:392] == snake[391:384] + 12 && snake[399:392] == prev_snake - 1)) // right-down / up-left
+                    else if ((snake[399:392] == snake[391:384] - 1 && snake[399:392] == prev_snake + 12) || (snake[399:392] == snake[391:384] + 12 && snake[399:392] == prev_snake - 1) || (snake[399:392] == snake[391:384] + 12 && snake[399:392] == prev_snake + 11) || (snake[399:392] == snake[391:384] + 11 && snake[399:392] == prev_snake + 12) || (snake[399:392] == snake[391:384] - 1 && snake[399:392] == prev_snake - 108) || (snake[399:392] == snake[391:384] - 108 && snake[399:392] == prev_snake - 1)) // left-up / down-right (3)
                         mark[snake[399:392]-1] <= 13;
-                    else if ((snake[399:392] == snake[391:384] + 1 && snake[399:392] == prev_snake + 12) || (snake[399:392] == snake[391:384] + 12 && snake[399:392] == prev_snake + 1)) // left-down / up-right
+                    else if ((snake[399:392] == snake[391:384] + 1 && snake[399:392] == prev_snake + 12) || (snake[399:392] == snake[391:384] + 12 && snake[399:392] == prev_snake + 1) || (snake[399:392] == snake[391:384] + 1 && snake[399:392] == prev_snake - 108) || (snake[399:392] == snake[391:384] - 108 && snake[399:392] == prev_snake + 1) || (snake[399:392] == snake[391:384] - 11 && snake[399:392] == prev_snake + 12) || (snake[399:392] == snake[391:384] + 12 && snake[399:392] == prev_snake - 11)) // right-up / down-left (4)
                         mark[snake[399:392]-1] <= 10;
-                    else if ((snake[399:392] == snake[391:384] + 1 && snake[399:392] == prev_snake - 12) || (snake[399:392] == snake[391:384] - 12 && snake[399:392] == prev_snake + 1)) // left-up / down-right
+                    else if ((snake[399:392] == snake[391:384] + 1 && snake[399:392] == prev_snake - 12) || (snake[399:392] == snake[391:384] - 12 && snake[399:392] == prev_snake + 1) || (snake[399:392] == snake[391:384] - 11 && snake[399:392] == prev_snake - 12) || (snake[399:392] == snake[391:384] - 12 && snake[399:392] == prev_snake - 11) || (snake[399:392] == snake[391:384] + 1 && snake[399:392] == prev_snake + 108) || (snake[399:392] == snake[391:384] + 108 && snake[399:392] == prev_snake + 1)) // right-down / up-left (1)
                         mark[snake[399:392]-1] <= 11;
                 end else if (snake[391:384] == 0) begin
                     change <= 2;
-                    if (snake[399:392] == prev_snake + 1) // tail right
+                    if ((snake[399:392] == snake[391:384] + 1) || snake[399:392] == snake[391:384] + 11) begin // tail right
                         mark[snake[399:392]-1] <= 6;
-                    else if (snake[399:392] == prev_snake - 1) // tail left
+                    end else if ((snake[399:392] == snake[391:384] - 1) || snake[399:392] == snake[391:384] - 11) begin // tail left
                         mark[snake[399:392]-1] <= 7;
-                    else if (snake[399:392] == prev_snake - 12) // tail up
+                    end else if ((snake[399:392] == snake[391:384] - 12) || snake[399:392] == snake[391:384] - 108) begin // tail up
                         mark[snake[399:392]-1] <= 8;
-                    else if (snake[399:392] == prev_snake + 12) // tail down
+                    end else if ((snake[399:392] == snake[391:384] + 12) || snake[399:392] == snake[391:384] + 108) begin // tail down
                         mark[snake[399:392]-1] <= 9;
+                    end
                 end
             end
         end else if (change == 2) begin
